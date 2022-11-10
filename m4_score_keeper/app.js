74f1685c217a4bf8b9104c072f49daabb83d5804
baselalmomani <83565574+baselalmomani@users.noreply.github.com>
2022-11-10 15:10:39 +0200

    Add files via upload

---
+++ m4_score_keeper/app.js
@@ -0,0 +1,87 @@
+const p1Points = document.getElementById("p1-points");
+const p2Points = document.getElementById("p2-points");
+const p1Btn = document.getElementById("p1-button");
+const p2Btn = document.getElementById("p2-button");
+const winnerContainer = document.getElementById("winner");
+
+let p1Score = 0;
+let p2Score = 0;
+const gameOver = 5;
+
+p1Btn.addEventListener("click" , function (event) {
+    addPoint(1);
+    if (p1Score >= gameOver){
+        endGame();
+    }
+});
+
+
+p2Btn.addEventListener("click" , function (event){
+    addPoint(2);
+    if (p2Score >= gameOver){
+        endGame();
+    }
+});
+
+
+document.getElementById("reset").addEventListener("click" , function () {
+    p1Score = 0;
+    p2Score = 0;
+    p1Points.textContent = 0;
+    p2Points.textContent = 0;
+    p1Btn.disabled = false;
+    p2Btn.disabled = false;
+    document.getElementById("winner").style.display = "none";
+    document.getElementsByTagName("body")[0].style.backgroundColor = "#fff";
+});
+
+function addPoint(player){
+    if (player == 1){
+        // add 1 point to the score
+        p1Score++;
+        if (p2Score > 0){
+            p2Score--;
+        } // print the score on the player points section
+        p1Points.textContent = p1Score;
+        p2Points.textContent = p2Score;
+    } else {
+        // add 1 point to the score
+        p2Score++;
+        if (p1Score > 0){
+            p1Score--;
+        } // print the score on the player points section
+        p2Points.textContent = p2Score;
+        p1Points.textContent = p1Score;
+    }
+}
+
+function endGame(){
+    let winnerPlayerName = null;
+
+    if (p1Score > p2Score){
+        winnerPlayerName = document.getElementById("p1-name").textContent;
+    } else {
+        winnerPlayerName = document.getElementById("p2-name").textContent;
+    }
+
+    document.getElementById("winner-name").textContent = winnerPlayerName;
+    winnerContainer.style.display = "block";
+    document.getElementsByTagName("body")[0].style.backgroundColor = "green";
+    p1Btn.disabled = true;
+    p2Btn.disabled = true;
+
+    let colorSwitch = 0;
+    setInterval( function () {
+
+        if(colorSwitch == 0){
+            winnerContainer.style.backgroundColor = "red";
+            colorSwitch++;
+        } else {
+            winnerContainer.style.backgroundColor = "yellow";
+            colorSwitch--;
+        }
+    }, 500);
+
+}
+
+
